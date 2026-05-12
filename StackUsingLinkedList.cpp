bd8ebafe16663d83228a1a17fe0d3c2d7b91076c
senteguhr <senteguhr@gmail.com>
2026-05-12 11:25:34 +0700

    Add exit menu functionality

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -118,10 +118,4 @@ int main()
             }
             break;
 
-        case 4:
-            cout << "Exiting program." << endl;
-            break;
-        default:
-            cout << "Invalid choice. Please try again." << endl;
-            break;
        
