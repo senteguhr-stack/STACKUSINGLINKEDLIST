d4d9a5089948c1d0181ffd6edf55c3e21e4be132
senteguhr <senteguhr@gmail.com>
2026-05-12 11:25:02 +0700

    Add invalid choice handling

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -118,4 +118,10 @@ int main()
             }
             break;
 
-        
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+            break;
+       
