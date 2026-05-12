9999d91dac20348240163fc2a270ad2e27fd50d2
senteguhr <senteguhr@gmail.com>
2026-05-12 11:24:26 +0700

    Implement peek menu option

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -107,4 +107,15 @@ int main()
                 cout << "Stack is Empty,cannot pop" << endl;
             }
             break;
-       
+        case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek();// get the value of the top element 
+            }
+            else
+            {
+                cout << "Stack is Empty, not top value" << endl;
+            }
+            break;
+
+        
