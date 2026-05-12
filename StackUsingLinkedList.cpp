d03805e2961c7f66ef16d9215860fe5d637a9e14
senteguhr <senteguhr@gmail.com>
2026-05-12 11:20:28 +0700

    Add linked list traversal in peek()

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -55,4 +55,14 @@ public:
         {
             cout << "List is Empty" << endl;
         }
-    
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
