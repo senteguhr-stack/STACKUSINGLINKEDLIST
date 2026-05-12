db30fd063dcb80529a9ea8ba9f56e4043c00232d
senteguhr <senteguhr@gmail.com>
2026-05-12 11:21:36 +0700

    Create menu display in main()

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -66,3 +66,26 @@ public:
             cout << endl;
         }
     }
+
+//
+bool isEmpty()
+{
+    return top == NULL;//
+}
+};
+
+int main()
+{
+    stack stack;
+
+    int choice = 0;
+    int value;
+
+    while (choice != 5)
+    {
+        cout << "1.push\n";
+        cout << "2.pop\n";
+        cout << "3.peek\n"; 
+        cout << "4.exit\n";
+        cout << "Enter your choice : ";
+        cin >> choice;
