ecc3425f367f711f4999a323db0ba0c4c7f933d7
senteguhr <senteguhr@gmail.com>
2026-05-12 11:12:17 +0700

    Implement push() function

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -25,3 +25,10 @@ public:
     {
         top = NULL;
     }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+    
